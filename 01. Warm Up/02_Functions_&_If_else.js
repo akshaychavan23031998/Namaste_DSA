ed4f7fff952c9fbfc4e2206d2f35896c503922ab
Akshay Ram Chavan <akshayrchavan07@gmail.com>
2025-10-02 18:58:27 +0530

    Completed Functions.

---
+++ 01. Warm Up/02_Functions_&_If_else.js
@@ -0,0 +1,34 @@
+function Helloworld() {
+  console.log("Hello World");
+}
+
+Helloworld();
+
+function Namaste(name) {
+  console.log("Namaste " + name);
+}
+
+Namaste("Akshay");
+Namaste("Rahul");
+
+function Sum(a, b) {
+  const total = a + b;
+  console.log("Sum " + total);
+}
+Sum(2, 3);
+Sum(10, 20);
+
+function Multiply(a, b) {
+  const total = a * b;
+  console.log("Multiply " + total);
+}
+Multiply(2, 3);
+Multiply(10, 20);
+
+function sequre(a) {
+  return a * a;
+}
+const squr = sequre(10);
+console.log(squr);
+const squr1 = sequre(8);
+console.log(squr1);
