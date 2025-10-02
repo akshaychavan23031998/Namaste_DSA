6cadf91016c1f445a843dcc06039476a39056347
Akshay Ram Chavan <akshayrchavan07@gmail.com>
2025-10-02 17:59:28 +0530

    Completed the 01_Programming_101.

---
+++ 01. Warm Up/01_Programming_101.js
@@ -0,0 +1,29 @@
+console.log("Namaste DSA");
+
+const a = 10;
+const b = 20;
+
+const sum = a + b;
+console.log(sum);
+
+let arr = [5, 10, 15, 20, 25, 30];
+
+console.log(arr);
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+
+const obj = {
+  name: "Akshay Chavan",
+  age: 27,
+  gender: "M",
+};
+
+console.log(obj);
+console.log(obj.name);
+console.log(obj.age);
+console.log(obj.gender);
