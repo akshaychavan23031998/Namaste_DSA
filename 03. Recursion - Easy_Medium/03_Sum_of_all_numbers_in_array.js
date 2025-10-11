fc822deabcc2486ae5fc85d316672a3f9d667b1d
Akshay Ram Chavan <akshayrchavan07@gmail.com>
2025-10-11 20:14:46 +0530

    Completed the sum of all odd elements.

--- 03. Recursion - Easy_Medium/03_Sum_of_all_numbers_in_array.js
+++ 03. Recursion - Easy_Medium/03_Sum_of_all_numbers_in_array.js
@@ -8,4 +8,14 @@ function sum(n) {
 }
 
 const res = sum(arr.length - 1);
-console.log(res);
+console.log(res); // 11
+
+function odd_sum(n) {
+  if (n == 0) {
+    return arr[0] % 2 ? arr[0] : 0;
+  }
+  return (arr[n] % 2 ? arr[n] : 0) + odd_sum(n - 1);
+}
+
+const output = odd_sum(arr.length - 1);
+console.log(output); // 9
