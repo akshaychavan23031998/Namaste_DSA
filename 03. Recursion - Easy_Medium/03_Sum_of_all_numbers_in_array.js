a1eed0a1d396636405d9eee5afd0290f609e92f5
Akshay Ram Chavan <akshayrchavan07@gmail.com>
2025-10-11 20:24:21 +0530

    With other array.

--- 03. Recursion - Easy_Medium/03_Sum_of_all_numbers_in_array.js
+++ 03. Recursion - Easy_Medium/03_Sum_of_all_numbers_in_array.js
@@ -1,4 +1,5 @@
 let arr = [5, 3, 2, 0, 1];
+let arr_odd = [5, 2, 0, 3, 6, 7];
 
 function sum(n) {
   if (n == 0) {
@@ -12,10 +13,10 @@ console.log(res); // 11
 
 function odd_sum(n) {
   if (n == 0) {
-    return arr[0] % 2 ? arr[0] : 0;
+    return arr_odd[0] % 2 ? arr_odd[0] : 0;
   }
-  return (arr[n] % 2 ? arr[n] : 0) + odd_sum(n - 1);
+  return (arr_odd[n] % 2 ? arr_odd[n] : 0) + odd_sum(n - 1);
 }
 
-const output = odd_sum(arr.length - 1);
-console.log(output); // 9
+const output = odd_sum(arr_odd.length - 1);
+console.log(output); // 15
