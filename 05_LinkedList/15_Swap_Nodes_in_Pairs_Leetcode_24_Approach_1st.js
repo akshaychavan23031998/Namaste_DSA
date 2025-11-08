a4abc418e950d0951ced7a678afcf07e3ac286da
Akshay Ram Chavan <akshayrchavan07@gmail.com>
2025-11-08 20:24:04 +0530

    Completed the leetcode 24 1st approach.

---
+++ 05_LinkedList/15_Swap_Nodes_in_Pairs_Leetcode_24_Approach_1st.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if(!head || !head.next) return head;
+
+    let dummy = new ListNode();
+    dummy.next = head;
+
+    let p = dummy;
+    let c = head;
+    let n = head.next;
+
+    while(c && n){
+        // swapping the nodes
+        p.next = n;
+        c.next = n.next;
+        n.next = c;
+
+        // updating the pointers
+        p = c;
+        c = p.next;
+        n = c && c.next;
+    }
+
+    return dummy.next;
+};
+
+// TC = O(n);
+// SC = O(1);
