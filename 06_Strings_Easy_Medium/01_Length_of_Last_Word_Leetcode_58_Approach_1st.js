b232bd0a2ade060da42efe601c435c4b11022ec3
Akshay Ram Chavan <akshayrchavan07@gmail.com>
2025-12-21 16:18:12 +0530

    Solved 58 in optimised way.

--- 06_Strings_Easy_Medium/01_Length_of_Last_Word_Leetcode_58_Approach_1st.js
+++ 06_Strings_Easy_Medium/01_Length_of_Last_Word_Leetcode_58_Approach_1st.js
@@ -28,3 +28,27 @@ var lengthOfLastWord = function(s) {
 
 // TC = O(n);
 // SC = O(1);
+
+
+// In Single Loop 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let n = s.length - 1;
+    let count = 0;
+    while(n>=0){
+        if(s[n] != " "){
+            count++;
+        }
+        else if(count > 0){
+            break;
+        }
+        --n;
+    }
+    return count;
+};
+
+// TC = O(n);
+// SC = O(1);
